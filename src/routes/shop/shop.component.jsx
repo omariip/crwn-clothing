1a843374cc03e268b0b7d8e029e11eae60fef7c3
omariip <aboushaar.omar@gmail.com>
2023-05-21 03:21:27 -0400

    added redux-thunk and implemented it in categories fetching

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,21 +3,16 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 const Shop = () => {
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    }
+      dispatch(fetchCategoriesAsync());
 
-    getCategoriesMap();
-  }, [dispatch])
+  }, [])
 
   return (
     <Routes>
--- src/store/store.js
+++ src/store/store.js
@@ -3,30 +3,34 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
+import thunk from 'redux-thunk';
 
 // root-reducer: combination of all reducers
 
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger,
+    thunk,
+].filter(Boolean);
 
-const composeEnhancer = 
-    (process.env.NODE_ENV !== 'production' && 
-    window && 
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+const composeEnhancer =
+    (process.env.NODE_ENV !== 'production' &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
     compose;
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
-    persistedReducer, 
-    undefined, 
+    persistedReducer,
+    undefined,
     composedEnhancers
 );
 
