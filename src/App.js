ef1e60f559bb02b14b610e5972cd9ac525dda579
omariip <aboushaar.omar@gmail.com>
2023-06-10 18:09:23 -0400

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { useEffect, lazy, Suspense } from "react";
-import Spinner from "./components/spinner/spinner.component";
-import { checkUserSession, setCurrentUser } from "./store/user/user.action";
+import { useEffect, lazy, Suspense } from 'react';
+import { useDispatch } from 'react-redux';
 
-const Shop = lazy(() => import("./routes/shop/shop.component"))
-const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
-const Navigation = lazy(() => import("./routes/shop/shop.component"))
-const Home = lazy(() => import("./routes/home/home.component"))
-const Authentication = lazy(() => import("./routes/authentication/authentication.component"))
+import { Routes, Route } from 'react-router-dom';
 
+import Spinner from './components/spinner/spinner.component';
+import { checkUserSession } from './store/user/user.action';
+
+const Navigation = lazy(() =>
+  import('./routes/navigation/navigation.component')
+);
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const Home = lazy(() => import('./routes/home/home.component'));
+const Authentication = lazy(() =>
+  import('./routes/authentication/authentication.component')
+);
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(checkUserSession());
-  }, [])
+  }, []);
 
   return (
     <Suspense fallback={<Spinner />}>
@@ -29,7 +34,7 @@ const App = () => {
         </Route>
       </Routes>
     </Suspense>
-  )
-}
+  );
+};
 
 export default App;
